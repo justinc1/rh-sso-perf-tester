db5e39560b3322e0a66db7f09a919b9cb2effddd
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-07-25 14:02:22 +0000

    Setup test user account for SSH login and sudo commands

--- README.md
+++ README.md
@@ -12,7 +12,10 @@ export APIURL=https://sso-minh-tran-duc-dev.apps.sandbox-m2.ll9k.p1.openshiftapp
 
 ./stress_test.py --url $APIURL --username admin --password admin_pass --workers 4 --requests 10
 ./normal_load.py --url $APIURL --username admin --password admin_pass --iter 3 --period 10 --action login
-./sys_command.py --hostname jcpc --username $USER --pkey $HOME/.ssh/id_rsa --command 'systemctl status' --service crond
+export SSHPASS=pass2
+# export SSHPASS=`cat`
+#   passphrase, enter, ctrl+d
+./sys_command.py --hostname localhost --username uu3 --pkey $HOME/.ssh/id_rsa2 --service crond --action restart --sudo_password uu3p
 
 # sample scenario - does not require SSO, only echo and similar commands are run
 ./sample_scenario_01.py main
@@ -21,4 +24,31 @@ export APIURL=https://sso-minh-tran-duc-dev.apps.sandbox-m2.ll9k.p1.openshiftapp
 export SSO_API_USERNAME=admin
 export SSO_API_PASSWORD=admin_pass
 ./scenario_02.py
+```
+
+## Setup test server
+
+We want to test only:
+ - SSH login with passphrase protected SSH key, passphrase is not provided by SSH agent.
+ - sudo with password
+
+SSH login:
+
+```shell
+# Create new ssh key, with passphrase.
+ssh-keygen -f ~/.ssh/id_rsa2 -N pass2 -C test-key-passphrase-pass2
+
+# Add new user, with sudo rights, sudo password required.
+sudo useradd -G wheel uu3
+echo uu3p | sudo passwd --stdin uu3
+sudo mkdir -m 0700 /home/uu3/.ssh
+sudo cat ~/.ssh/id_rsa2.pub | sudo dd of=/home/uu3/.ssh/authorized_keys
+sudo chmod 0600 /home/uu3/.ssh/authorized_keys
+sudo chown -R uu3:uu3 /home/uu3/.ssh
+# sudo userdel -r uu3
+
+# Test noninteractive login
+export SSHPASS=pass2
+unset SSH_AUTH_SOCK
+sshpass -e -v -P passphrase ssh -i ~/.ssh/id_rsa2 uu3@localhost date
 ```
--- sys_command.py
+++ sys_command.py
@@ -1,51 +1,71 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import paramiko
 import argparse
+import logging
 
 output_file = 'out_paramiko.txt'
-port = '22'
 
 
 def parse_args():
-
-    parser = argparse.ArgumentParser(description='Retrieve args')
-    parser.add_argument('--hostname', required=True)
-    parser.add_argument('--username', required=False, default=os.environ['USER'])
-    parser.add_argument('--pkey', required=False)
-    parser.add_argument('--command', required=True)
-    parser.add_argument('--service', required=True)
+    parser = argparse.ArgumentParser(
+        description='Execute commands via SSH.'
+                    ' Helps to login with passphrase protected SSH key, non-interactively.')
+    # SSH connection paramters
+    parser.add_argument('--hostname', required=True,
+                        help='Host to SSH to')
+    parser.add_argument('--port', type=int, default=22,
+                        help='SSH port')
+    parser.add_argument('--username', required=False, default=os.environ['USER'],
+                        help='SSH username')
+    parser.add_argument('--pkey', required=True,
+                        help='SSH private key')
+    parser.add_argument('--passphrase', required=False, default=os.environ.get("SSHPASS"),
+                        help='Passphrase for SSH key. By default environ variable SSHPASS will be used (if set).')
+    # Parameters for executed command
+    parser.add_argument('--service', required=True,
+                        help='Systemd service to control')
+    parser.add_argument('--action', required=True, choices=['start', 'stop', 'restart', 'status'],
+                        help='Action to apply to systemd service')
+    parser.add_argument('--sudo_password', required=True,
+                        help='Password to be used for sudo.')
     args = parser.parse_args()
     return args
 
-def connection():
 
-    args = parse_args()
+def connection(hostname, port, username, pkey, passphrase):
     client = paramiko.client.SSHClient()
     policy = paramiko.AutoAddPolicy()
     client.set_missing_host_key_policy(policy)
-    # client.connect(hostname=host, username=username, password=password, allow_agent=False, look_for_keys=False)
-    # client.connect(hostname=args.hostname, port=port, username=args.username, key_filename=args.pkey, allow_agent=False, look_for_keys=False)
-
-    # with allow_agent=True, the key_filename is not really used
-    client.connect(hostname=args.hostname, port=port, username=args.username, key_filename=args.pkey, allow_agent=True, look_for_keys=False)
-
+    pkey_decrypted_content = paramiko.RSAKey.from_private_key(open(pkey), passphrase)
+    client.connect(hostname=hostname, port=port, username=username, pkey=pkey_decrypted_content, allow_agent=False, look_for_keys=False)
     return client
 
 
 def main():
-
-    client = connection()
     args = parse_args()
-    cmd = [args.command, args.service]
-    execute = ' '.join(cmd)
-    (stdin, stdout, stderr) = client.exec_command(execute)
-    # (stdin, stdout, stderr) = client.exec_command("sudo systemctl status crond")
+    client = connection(args.hostname, args.port, args.username, args.pkey, args.passphrase)
+    ssh_param_description = f"hostname={args.hostname} port={args.port} username={args.username} pkey={args.pkey}"
+    print(f"SSH connection: {ssh_param_description}")
 
-    cmd_output = stdout.read().decode()
-    print('log printing: ', cmd_output)
+    systemctl_cmd = ['systemctl', args.action, args.service]
+    sudo_cmd = ['sudo', '-S']
+    # execute = ' '.join(sudo_cmd + ['whoami'])  # a safe test
+    execute = ' '.join(sudo_cmd + systemctl_cmd)
+    print(f"Command: {execute}")
+
+    # https://stackoverflow.com/questions/48554497/how-do-i-write-to-stdin-returned-from-exec-command-in-paramiko
+    (stdin, stdout, stderr) = client.exec_command(execute)
+    stdin.channel.send(args.sudo_password + "\n")
+    stdin.channel.shutdown_write()
+
+    stdout_content = stdout.read().decode()
+    stderr_content = stderr.read().decode()
     print(f'exit code: {stdout.channel.recv_exit_status()}')
+    print('stdout_content: ', stdout_content)
+    print('stderr_content: ', stderr_content)
 
     # TODO what we want in output file:
     #   time of execution
@@ -55,7 +75,8 @@ def main():
     # Maybe call should be
     # ./sys_command.py --hostname jcpc --username root --pkey $HOME/.ssh/id_rsa --service crond --action start
     with open(output_file, "w+") as file:
-        file.write(str(cmd_output))
+        file.write(str(stdout_content))
+        file.write(str(stderr_content))
 
     client.close()
 
